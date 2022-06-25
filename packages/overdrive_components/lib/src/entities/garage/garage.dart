96611a9ac111066040598e5f97b6f5d09a8f4630
Luan Nico <luanpotter27@gmail.com>
2022-06-24 23:17:58 -0400

    Render garage

--- packages/overdrive_components/lib/src/entities/garage/garage.dart
+++ packages/overdrive_components/lib/src/entities/garage/garage.dart
@@ -5,9 +5,23 @@ import 'package:flame_behaviors/flame_behaviors.dart';
 
 class Garage extends Entity with HasGameRef {
   static final _paint = BasicPalette.white.paint();
+  static final _carSpotPaint = BasicPalette.red.paint();
 
   @override
   void render(Canvas canvas) {
+    renderBackground(canvas);
+    renderCarSpots(canvas);
+  }
+
+  void renderBackground(Canvas canvas) {
     canvas.drawRect(gameRef.size.toRect().deflate(32.0), _paint);
+  }
+
+  void renderCarSpots(Canvas canvas) {
+    final height = gameRef.size.y / 5;
+    final width = 320.0;
+    final x = (gameRef.size.x - width) / 2;
+    canvas.drawRect(Rect.fromLTWH(x, height, width, height), _carSpotPaint);
+    canvas.drawRect(Rect.fromLTWH(x, 3 * height, width, height), _carSpotPaint);
   }
 }
--- packages/overdrive_components/sandbox/lib/main.dart
+++ packages/overdrive_components/sandbox/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:dashbook/dashbook.dart';
 import 'package:flutter/material.dart';
+import 'package:sandbox/stories/garage/garage.dart';
 import 'package:sandbox/stories/stories.dart';
 
 void main() {
   final dashbook = Dashbook(theme: ThemeData.light());
 
   addCarStories(dashbook);
+  addGarageStories(dashbook);
   addPlayerStories(dashbook);
 
   runApp(dashbook);
