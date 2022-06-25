28fe791c7f54c134e89287e1c3dc241bf9a33c36
Luan Nico <luanpotter27@gmail.com>
2022-06-24 20:34:15 -0400

    Empty game

---
+++ game/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    // TODO: load stuff
+  }
+}
