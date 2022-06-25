ba82097d46c1cf9ac7438341364ab8bbb908356f
Luan Nico <luanpotter27@gmail.com>
2022-06-25 14:12:46 -0400

    feat: Wire up player and toolbox assets (#35)

--- packages/overdrive_components/lib/src/entities/player/player_body_component.dart
+++ packages/overdrive_components/lib/src/entities/player/player_body_component.dart
@@ -1,44 +1,43 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
-import 'package:flame/palette.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:overdrive_components/src/utils.dart';
 
 import 'player.dart';
 
 class PlayerBodyComponent extends BodyComponent with ParentIsA<Player> {
-  static final double size = 4.0;
-  static final _paint = BasicPalette.blue.paint();
+  // original sprite size: 468 x 613
+  static final Vector2 size = Vector2(4.68, 6.13);
 
+  late final Sprite sprite;
   final Vector2 startPosition;
 
-  PlayerBodyComponent(
-    this.startPosition,
-  ) : super(
-          children: [
-            RectangleComponent.relative(
-              Vector2.all(1),
-              parentSize: Vector2.all(size),
-              paint: _paint,
-            ),
-          ],
-        );
+  PlayerBodyComponent(this.startPosition);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    sprite = await Sprite.load('player.png');
+  }
+
+  @override
+  void render(Canvas canvas) {
+    sprite.render(canvas, size: size);
+  }
 
   @override
   Body createBody() {
     final def = BodyDef()
       ..type = BodyType.dynamic
+      ..fixedRotation = true
       ..position = startPosition;
     final body = world.createBody(def)
       ..userData = this
-      ..angularDamping = 10.0
       ..linearDamping = 2.0;
 
-    final vertices = [
-      Vector2(0, 0),
-      Vector2(0, size),
-      Vector2(size, size),
-      Vector2(size, 0),
-    ];
-    final shape = PolygonShape()..set(vertices);
+    final shape = size.toRect().toPolygonShape();
     final fixtureDef = FixtureDef(shape)
       ..density = 0.0002
       ..restitution = 0.5;
