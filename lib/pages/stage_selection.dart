96497a62358ae385f20f7145cba3d814f50abc6f
Tyemy Kuga <tyemykuga@gmail.com>
2022-06-26 12:40:31 -0300

    feat: Adding theme colors and adjusting title screen (#59)
    Adding theme colors and adjusting title screen

--- lib/pages/stage_selection.dart
+++ lib/pages/stage_selection.dart
@@ -4,46 +4,75 @@ class StageSelection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text(
-            'Stages',
-            style: TextStyle(fontSize: 30),
-          ),
-          Expanded(
-            child: Center(
-              child: SizedBox(
-                width: 800,
-                child: GridView.count(
-                  padding: const EdgeInsets.all(50),
-                  crossAxisCount: 5,
-                  mainAxisSpacing: 30,
-                  crossAxisSpacing: 30,
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.of(context).pushNamed('/game'),
-                      child: Container(
-                        color: Colors.blue,
-                        child: const Center(
-                          child: Text('1'),
-                        ),
-                      ),
-                    ),
-                    for (int i = 2; i <= 20; i++)
-                      Container(
-                        color: Colors.blue,
-                        child: const Center(
-                          child: Icon(
-                            Icons.lock,
-                          ),
-                        ),
-                      ),
-                  ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).pushNamed('/'),
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 50,
                 ),
               ),
             ),
-          ),
-        ],
+            const Text(
+              'Stages',
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: SizedBox(
+                  width: 800,
+                  child: GridView.count(
+                    padding: const EdgeInsets.all(50),
+                    crossAxisCount: 5,
+                    mainAxisSpacing: 30,
+                    crossAxisSpacing: 30,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.of(context).pushNamed('/game'),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color(0XFF9575cd),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '1',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 36,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      for (int i = 2; i <= 20; i++)
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0XFF55018C),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.lock,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
