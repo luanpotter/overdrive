fecc42011f9726755300729eb10a2671cbb26ee2
tyemy <tyemykuga@gmail.com>
2022-06-26 12:28:31 -0300

    Adding theme colors and adjusting title screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,69 @@ import 'package:overdrive/pages/stage_selection.dart';
 import 'package:overdrive/pages/title_screen.dart';
 import 'package:overdrive_components/overdrive_components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 void main() {
   Flame.images.prefix = '';
 
   runApp(
     MaterialApp(
+      theme: overdriveTheme,
       routes: {
         '/': (context) => TitleScreen(),
         '/stages': (context) => StageSelection(),
         '/game': (context) => GameWidget(game: OverdriveGame()),
       },
     ),
+  );
+}
+
+ThemeData get overdriveTheme {
+  ColorScheme flexSchemeDark() => ColorScheme(
+        brightness: Brightness.dark,
+        primary: Color(0xff55018C),
+        onPrimary: Color(0xfff5f6fe),
+        primaryContainer: Color(0xff283593),
+        onPrimaryContainer: Color(0xffd9dcf2),
+        secondary: Color(0xff9575cd),
+        onSecondary: Color(0xfff8f4ff),
+        secondaryContainer: Color(0xff502ca7),
+        onSecondaryContainer: Color(0xffe3daf7),
+        tertiary: Color(0xff7e57c2),
+        onTertiary: Color(0xfff5f1fd),
+        tertiaryContainer: Color(0xff4433a4),
+        onTertiaryContainer: Color(0xffe0dcf6),
+        error: Color(0xffcf6679),
+        onError: Color(0xff1e1214),
+        errorContainer: Color(0xffb1384e),
+        onErrorContainer: Color(0xfff9dde2),
+        outline: Color(0xff959599),
+        background: Color(0xff17171b),
+        onBackground: Color(0xffe3e3e4),
+        surface: Color(0xff131416),
+        onSurface: Color(0xfff1f1f1),
+        surfaceVariant: Color(0xff16171b),
+        onSurfaceVariant: Color(0xffe3e3e4),
+        inverseSurface: Color(0xfffbfbfd),
+        onInverseSurface: Color(0xff0e0e0e),
+        inversePrimary: Color(0xff414663),
+        shadow: Color(0xff000000),
+      );
+
+  return FlexThemeData.light(
+    scheme: FlexScheme.indigo,
+    scaffoldBackground: Color(0xff060642),
+    colorScheme: flexSchemeDark(),
+    surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+    blendLevel: 20,
+    appBarOpacity: 0.95,
+    subThemesData: const FlexSubThemesData(
+      blendOnLevel: 20,
+      blendOnColors: false,
+      elevatedButtonRadius: 10,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true,
   );
 }
 
--- lib/pages/title_screen.dart
+++ lib/pages/title_screen.dart
@@ -5,55 +5,59 @@ import 'package:overdrive_components/overdrive_components.dart';
 class TitleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          height: 320,
-          width: 500,
-          decoration: BoxDecoration(
-              border: Border.all(color: Color(0xFF4A86E8), width: 10),
-              borderRadius: BorderRadius.circular(16),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRect(
-              child: GameWidget(
-                game: TitleAnimationPlayer(),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 320,
+              width: 500,
+              decoration: BoxDecoration(
+                color: Color(0xffC2D0E8),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: ClipRect(
+                  child: GameWidget(
+                    game: TitleAnimationPlayer(),
+                  ),
+                ),
               ),
             ),
-          ),
+            const SizedBox(
+              height: 24,
+            ),
+            SizedBox(
+              width: 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    child: const Text('Play'),
+                    onPressed: () => Navigator.of(context).pushNamed('/stages'),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    child: const Text('Settings'),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    child: const Text('Credits'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        const SizedBox(
-          height: 24,
-        ),
-        SizedBox(
-          width: 150,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ElevatedButton(
-                child: const Text('Play'),
-                onPressed: () => Navigator.of(context).pushNamed('/stages'),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              ElevatedButton(
-                child: const Text('Settings'),
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              ElevatedButton(
-                child: const Text('Credits'),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
