46900d01967c97b30305e4d3be64e5ac635b647a
Elias Reis <eliasreis54@gmail.com>
2022-06-25 22:32:28 -0300

    chore(tire-fixer-workbench-asset): wire up the tire fixer workbench asset

--- packages/overdrive_components/lib/src/entities/tire_fixer_workbench/tire_fixer_workbench_body_component.dart
+++ packages/overdrive_components/lib/src/entities/tire_fixer_workbench/tire_fixer_workbench_body_component.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'package:overdrive_components/gen/assets.gen.dart';
 import 'package:overdrive_components/src/entities/entities.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:overdrive_components/src/utils.dart';
@@ -8,6 +9,7 @@ class TireFixerWorkbenchBodyComponent extends BodyComponent
     with ParentIsA<TireFixerWorkbench> {
   static final size = Vector2(6, 3);
 
+  late final Sprite sprite;
   final Vector2 startPosition;
 
   TireFixerWorkbenchBodyComponent({
@@ -15,8 +17,15 @@ class TireFixerWorkbenchBodyComponent extends BodyComponent
   });
 
   @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    sprite = await Sprite.load(Assets.images.tireFixerWorkbench.keyName);
+  }
+
+  @override
   void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _tireFixerWorkbenchPaint);
+    sprite.render(canvas, size: size);
   }
 
   @override
