35bbc4170ebbf1bc9426b676ef65ee8684b53652
Erick <erickzanardoo@gmail.com>
2022-06-26 15:36:06 -0300

    feat: car spot pattern (#62)
    * feat: car spot pattern
    
    * format

---
+++ packages/overdrive_components/lib/src/entities/car_spot.dart
@@ -0,0 +1,22 @@
+import 'package:flame/components.dart';
+import 'package:overdrive_components/gen/assets.gen.dart';
+
+class CarSpot extends SpriteComponent with HasGameRef {
+  CarSpot({required this.top});
+
+  final bool top;
+
+  @override
+  Future<void> onLoad() async {
+    height = gameRef.size.y / 5;
+    width = 32.0;
+    x = (gameRef.size.x - width) / 2;
+    if (top) {
+      y = height;
+    } else {
+      y = 3 * height;
+    }
+
+    sprite = await Sprite.load(Assets.images.carSpot.keyName);
+  }
+}
--- packages/overdrive_components/lib/src/entities/garage/garage.dart
+++ packages/overdrive_components/lib/src/entities/garage/garage.dart
@@ -1,17 +1,10 @@
-import 'dart:ui';
-
 import 'package:flame/components.dart';
-import 'package:flame/extensions.dart';
-import 'package:flame/palette.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:overdrive_components/overdrive_components.dart';
 
 import 'garage_border_body.dart';
 
 class Garage extends Entity with HasGameRef {
-  static final _paint = BasicPalette.white.paint();
-  static final _carSpotPaint = Paint()..color = const Color(0xFFCCCCCC);
-
   @override
   Future<void> onLoad() async {
     await super.onLoad();
@@ -26,23 +19,8 @@ class Garage extends Entity with HasGameRef {
     );
 
     add(GarageFloor(position: delta, size: size));
-  }
 
-  @override
-  void renderTree(Canvas canvas) {
-    super.renderTree(canvas);
-    renderCarSpots(canvas);
-  }
-
-  void renderBackground(Canvas canvas) {
-    canvas.drawRect(gameRef.size.toRect().deflate(4.0), _paint);
-  }
-
-  void renderCarSpots(Canvas canvas) {
-    final height = gameRef.size.y / 5;
-    final width = 32.0;
-    final x = (gameRef.size.x - width) / 2;
-    canvas.drawRect(Rect.fromLTWH(x, height, width, height), _carSpotPaint);
-    canvas.drawRect(Rect.fromLTWH(x, 3 * height, width, height), _carSpotPaint);
+    add(CarSpot(top: true));
+    add(CarSpot(top: false));
   }
 }
