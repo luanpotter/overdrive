b1adcc45f1cc111e77d79750d078641929db34a0
Elias Reis <eliasreis54@gmail.com>
2022-06-26 01:46:40 -0300

    chore(tool-table-hold-item): hold item

---
+++ packages/overdrive_components/lib/src/entities/items/items.dart
@@ -0,0 +1,3 @@
+export 'item_type.dart';
+export 'pneumatic_screwdriver/pneumatic_screwdriver.dart';
+export 'tire/tire.dart';
--- packages/overdrive_components/lib/src/entities/player/behaviors/keyboard_movement_behavior.dart
+++ packages/overdrive_components/lib/src/entities/player/behaviors/keyboard_movement_behavior.dart
@@ -8,6 +8,9 @@ import 'package:overdrive_components/src/entities/items/item_type.dart';
 import 'package:overdrive_components/src/utils.dart';
 
 const _MAX_PICKUP_DISTANCE = 7.5;
+const _MIN_DROPOFF_DISTANCE = 10.5;
+
+typedef double MapCallback<T>(Body player, T item);
 
 class KeyboardMovementBehavior extends Behavior<Player>
     with KeyboardHandler, HasGameRef {
@@ -95,21 +98,47 @@ class KeyboardMovementBehavior extends Behavior<Player>
       final player = parent.body.body;
       final currentHoldingItem = parent.holdingItem;
       if (currentHoldingItem != null) {
-        final entity = currentHoldingItem.spawn(
-          position: player.position + Vector2(2.0, 0),
-          physics: true,
+        final closestToolTable = findThings<ToolTable>(
+          (_) => true,
+          (player, item) => _computeToolTableDistance(player, item),
+          player,
         );
-        parent.gameRef.add(entity);
+
+        if (closestToolTable != null &&
+            closestToolTable.value <= _MIN_DROPOFF_DISTANCE) {
+          closestToolTable.key.holdingItem = currentHoldingItem;
+        } else {
+          final entity = currentHoldingItem.spawn(
+            position: player.position + Vector2(2.0, 0),
+            physics: true,
+          );
+          parent.gameRef.add(entity);
+        }
+
         parent.holdingItem = null;
       } else {
-        final closestItem = parent.gameRef.children
-            .whereType<ItemEntity>()
-            .where((item) => item.realPosition != null)
-            .map((item) => Pair(item, _computePickupDistance(player, item)))
-            .minOrNullBy((pair) => pair.value);
+        final closestItem = findThings<ItemEntity>(
+          (item) => (item as ItemEntity).realPosition != null,
+          (player, item) => _computePickupDistance(player, item),
+          player,
+        );
+
         if (closestItem != null && closestItem.value <= _MAX_PICKUP_DISTANCE) {
           parent.holdingItem = closestItem.key.itemType;
           parent.gameRef.remove(closestItem.key);
+        }
+
+        final closestToolTable = findThings<ToolTable>(
+          (_) => true,
+          (player, item) => _computeToolTableDistance(player, item),
+          player,
+        );
+
+        if (closestToolTable != null &&
+            closestToolTable.value <= _MAX_PICKUP_DISTANCE &&
+            closestToolTable.key.holdingItem != null) {
+          parent.holdingItem = closestToolTable.key.holdingItem;
+          closestToolTable.key.holdingItem = null;
         }
       }
     }
@@ -119,6 +148,17 @@ class KeyboardMovementBehavior extends Behavior<Player>
   static double _computePickupDistance(Body player, ItemEntity item) {
     return item.realPosition!.distanceTo(player.position);
   }
+
+  static double _computeToolTableDistance(Body player, ToolTable item) {
+    return item.body.body.position.distanceTo(player.position);
+  }
+
+  Pair? findThings<T>(whereCallBack, MapCallback mapCallBack, Body player) =>
+      parent.gameRef.children
+          .whereType<T>()
+          .where(whereCallBack)
+          .map((item) => Pair(item, mapCallBack(player, item)))
+          .minOrNullBy((p0) => p0.value);
 
   @override
   void update(double dt) {
